7612344cb9f0ed5d7c9419bb331f647ace934e25
Dario <dario.ritacco87@gmail.com>
2023-07-12 21:00:23 -0300

    main.js point 1 & 3 complete

--- code/src/js/main.js
+++ code/src/js/main.js
@@ -1,39 +1,28 @@
-
 // 1: Leo title y lo imprimo por consola
 let title = document.querySelector("title");
 console.log(title.innerHTML);
 
-// 3: 
+// 3: Mostrar el nombre completo de cada integrante:
 
-let listaDl = document.querySelector("dd");
-console.log(listaDl.textContent);
+//Cargo en variable los valores de dl
+let listaIntegrantes = document.querySelectorAll("dl");
 
+//itero en for para los dos integrantes
+for (let indice = 0; indice < listaIntegrantes.length; indice++) {
+  let dl = listaIntegrantes[indice];
+  let etiDD = dl.querySelectorAll("dd");
+  //array vacio para ir almacenando nombre/apellido
+  let valores = [];
 
-var listaDescriptiva = document.querySelectorAll("dl");console.log(listaDescriptiva);
-
-
-
-
-/*
-document.addEventListener("DOMContentLoaded", function() {
-    var listaDescriptiva = document.querySelectorAll("dl");
-  
-    listaDescriptiva.forEach(function(dl, index) {
-      var etiquetasDT = dl.querySelectorAll("dt");
-      var etiquetasDD = dl.querySelectorAll("dd");
-      
-      var valores = [];
-      
-      for (var i = 0; i < etiquetasDT.length; i++) {
-        var dd = etiquetasDD[i].textContent;
-        
-        if (dd.trim() !== "") {
-          valores.push(dd);
-        }
-      }
-      
-      console.log("Integrante " + (index + 1) + ": " + valores.join(" "));
-    });
-  });
-  
-  */
+  //itero los cuatro valores de los dd
+  for (let i = 0; i < etiDD.length; i++) {
+    //guardo el nombre en cada iteracion
+    let dd = etiDD[i].textContent;
+    //pusheo los valores que no están nulos en el array
+    if (dd.trim() !== "") {
+      valores.push(dd);
+    }
+  }
+  //logueo los integrantes separando con espacio
+  console.log("Integrante "+(indice+1)+": "+valores.join(" "));
+}
